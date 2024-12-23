b0267015cd326a538f4481f723f7eeac2bcfb8e1
mahima9728 <mahimarawat979@gmail.com>
2024-12-23 12:54:52 +0530

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,17 @@ import StopWatch from "./StopWatch";
 
 function App() {
   return (
-      <Router basename="/React-mini-projects">
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/color-picker" element={<ColorPicker />} />
             <Route path="/digital-watch" element={<DigitalClock />} />
-            <Route path="/stopwatch" element={<StopWatch />} />
+        <Route path="/stopwatch" element={<StopWatch />} />
+        <Route path="*" element={<Home />} />
+
           </Routes>
         </div>
-      </Router>
+      
   );
 }
 
